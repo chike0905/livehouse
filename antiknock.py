138e5fa57677a9e57e4c35f455f374a351ea7fd5
ryosuke <ryosuke@net75-dhcp59.sfc.keio.ac.jp>
2015-11-26 16:43:49 +0900

    スクレイピング部分を関数へ切り分け

--- antiknock.py
+++ antiknock.py
@@ -7,30 +7,36 @@ import networkx as nx
 import pylab
 import matplotlib.pyplot as plt
 
+def eventjson(html):
+  soup = BeautifulSoup(html,"html.parser")
+
+  event_ttls = soup.find_all("div",class_="sche_box_right")
+  events = []
+  for event_ttl in event_ttls:
+    event_title = event_ttl.find("div",class_="sche_event_ttl")
+    if event_title.a is not None:
+      title = event_ttl.a.get_text(strip=True)
+      artists = event_ttl.find("div",class_="artist_text")
+      if artists is not None:
+        event = {}
+        artist = artists.get_text(strip=True)
+        artist_lists = artist.split("/")
+
+        artist_list = []
+        for artist_name in artist_lists:
+          artist_list.append(artist_name.strip())
+
+        event["title"] = title
+        event["artist"] = artist_list
+        events.append(event)
+  return events
+
+events = []
 #スクレイピングするURLを指定
 #HTML取得,soupオブジェクト生成
 html = urllib2.urlopen(u"http://www.antiknock.net/schedule/")
-soup = BeautifulSoup(html,"html.parser")
+events = events + eventjson(html)
 
-event_ttls = soup.find_all("div",class_="sche_box_right")
-events = []
-for event_ttl in event_ttls:
-  event_title = event_ttl.find("div",class_="sche_event_ttl")
-  if event_title.a is not None:
-    title = event_ttl.a.get_text(strip=True)
-    artists = event_ttl.find("div",class_="artist_text")
-    if artists is not None:
-      event = {}
-      artist = artists.get_text(strip=True)
-      artist_lists = artist.split("/")
-
-      artist_list = []
-      for artist_name in artist_lists:
-        artist_list.append(artist_name.strip())
-
-      event["title"] = title
-      event["artist"] = artist_list
-      events.append(event)
 
 '''
 以下データ整形
@@ -84,7 +90,7 @@ for artist in artist_list:
     if artistdata["name"] is artist:
       #networkが存在するartist毎に処理
       for w_artists in artistdata["network"].keys():
-        G.add_edge(artist,w_artists)
+        G.add_edge(artist,w_artists,weight=artistdata["network"][w_artists])
 
 
 #レイアウトの最適化
