5906219e45b67f6be94199f1140aebf602a33238
ryosuke <ryosuke@net75-dhcp59.sfc.keio.ac.jp>
2015-11-26 15:41:12 +0900

    ネットワークを描写

--- antiknock.py
+++ antiknock.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import urllib2
 import HTMLParser
 import json
+import networkx as nx
+import pylab
+import matplotlib.pyplot as plt
 
 #スクレイピングするURLを指定
 #HTML取得,soupオブジェクト生成
@@ -29,29 +32,68 @@ for event_ttl in event_ttls:
       event["artist"] = artist_list
       events.append(event)
 
-#以下データ整形
+'''
+以下データ整形
+arttist_list = ["バンド名"]
+artist=[{
+  "name":バンド名,
+  "network":
+    "バンド名":対バン回数
+  }
+]
+'''
 artists = []
+artist_list = []
 for event in events:
   for artist in event["artist"]:
-    #すでにartistsに情報があるか判定
-    if artist not in artists:
-      w_artists = []
-      #artistが出てるイベントを抽出
-      #2015.11.25 TODO
-      #自分が含まれるイベントが複数あった時、一つは自分を含んだままになってしまう
-      for eventdata in events:
-        if artist in eventdata["artist"]:
-          for eventartist in eventdata["artist"]:
-            if eventartist is not artist:
-              w_artists.append(eventartist)
+    #artist名が長すぎるものを弾く
+    if len(artist) < 50:
+      #すでにartistsに情報があるか判定
+      if artist not in artists:
+        if artist not in artist_list:
+          artist_list.append(artist)
+        w_artists = []
+        #artistが出てるイベントを抽出
+        for eventdata in events:
+          if artist in eventdata["artist"]:
+            for eventartist in eventdata["artist"]:
+              if eventartist is not artist:
+                w_artists.append(eventartist)
 
-      network = {}
-      for w_artist in w_artists:
-        if w_artist not in network:
-          network[w_artist] = w_artists.count(w_artist)
-      artistdata = {}
-      artistdata["name"] = artist
-      artistdata["network"] = network
-      artists.append(artistdata)
+        network = {}
+        for w_artist in w_artists:
+          if w_artist not in network:
+            network[w_artist] = w_artists.count(w_artist)
+        artistdata = {}
+        artistdata["name"] = artist
+        artistdata["network"] = network
+        artists.append(artistdata)
 
-print json.dumps(artists,ensure_ascii=False,indent=4)
+#グラフオブジェクトの作成
+G = nx.Graph()
+
+#頂点の作成
+for artist in artist_list:
+  G.add_node(artist)
+
+#エッジの作成
+#artist_listよりartist毎に処理
+for artist in artist_list:
+  #artistsよりartistのデータを抽出
+  for artistdata in artists:
+    if artistdata["name"] is artist:
+      #networkが存在するartist毎に処理
+      for w_artists in artistdata["network"].keys():
+        G.add_edge(artist,w_artists)
+
+
+#レイアウトの最適化
+pos = nx.spring_layout(G)
+
+nx.draw_networkx_nodes(G, pos, node_size=200, node_color="w")
+nx.draw_networkx_edges(G, pos, width=2)
+nx.draw_networkx_labels(G, pos ,font_size=8, font_color="r")
+
+# 表示
+plt.show()
+
