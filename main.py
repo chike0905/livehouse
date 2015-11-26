54e16414bdf6efe3425ae6de6ae7889d7284e81d
ryosuke <ryosuke@net75-dhcp59.sfc.keio.ac.jp>
2015-11-26 22:26:04 +0900

    scrapingの別ファイル化

--- main.py
+++ main.py
@@ -1,45 +1,19 @@
 # coding: utf-8
-from bs4 import BeautifulSoup
 import urllib2
-import HTMLParser
 import json
 import networkx as nx
 import pylab
 import matplotlib.pyplot as plt
-
-def eventjson(html):
-  soup = BeautifulSoup(html,"html.parser")
-
-  event_ttls = soup.find_all("div",class_="sche_box_right")
-  events = []
-  for event_ttl in event_ttls:
-    event_title = event_ttl.find("div",class_="sche_event_ttl")
-    if event_title.a is not None:
-      title = event_ttl.a.get_text(strip=True)
-      artists = event_ttl.find("div",class_="artist_text")
-      if artists is not None:
-        event = {}
-        artist = artists.get_text(strip=True)
-        artist_lists = artist.split("/")
-
-        artist_list = []
-        for artist_name in artist_lists:
-          artist_list.append(artist_name.strip())
-
-        event["title"] = title
-        event["artist"] = artist_list
-        events.append(event)
-  return events
+import scraping as sc
 
 events = []
-
 #antiknockの2月以前が取得できないため3~12月に設定
 for i in range(3,12):
   #スクレイピングするURLを指定
   #HTML取得,soupオブジェクト生成
   url = u"http://www.antiknock.net/schedule/2015/" + str(i)
   html = urllib2.urlopen(url)
-  events = events + eventjson(html)
+  events = events + sc.antiknock(html)
 
 
 '''
@@ -107,4 +81,3 @@ nx.draw_networkx_labels(G, pos ,font_size=8, font_color="r")
 
 # 表示
 plt.show()
-
